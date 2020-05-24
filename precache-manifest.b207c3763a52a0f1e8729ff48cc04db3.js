c6245cb976bf5a25b25067841116f88c4bfb1853
tutu-tonton <tngt33844507@gmail.com>
2020-05-24 19:33:41 +0900

    Updates

---
+++ precache-manifest.b207c3763a52a0f1e8729ff48cc04db3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5033c18fad413cd71b109a71a44f56",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d15968a584c4e82b84e6",
+    "url": "/monsters-rolodex/static/css/main.7a992480.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d15968a584c4e82b84e6",
+    "url": "/monsters-rolodex/static/js/main.b81b4699.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
